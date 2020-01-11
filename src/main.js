d764c098a12395b2bbb5fed27be1a36c8eddfdab
maomao <466597085@qq.com>
2020-01-11 17:46:03 +0800

    安装vant以及rem适配完成

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vant from 'vant'
-import 'vant/lib/index.css'
-import 'amfe-flexible'
-Vue.use(Vant)
+import Vant from 'vant' // 引入vant组件库
+import 'vant/lib/index.css' // vant样式文件
+import 'amfe-flexible' // 引入自动适配插件
+Vue.use(Vant) // 完成vant全局注册
 Vue.config.productionTip = false
 
 new Vue({
