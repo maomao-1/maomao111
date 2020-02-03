769bc214d953a8d98fc6218a8802bddaf70273e2
maomao <466597085@qq.com>
2020-02-04 01:04:32 +0800

    完成页面的最初始搭建，三图单图模式

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -2,9 +2,32 @@
   <!-- 这里注意 这个div设置了滚动条 目的是 给后面做 阅读记忆 留下伏笔 -->
   <!-- 阅读记忆 => 看文章看到一半 滑到中部 去了别的页面 当你回来时 文章还在你看的位置 -->
   <div class="scroll-wrapper">
-    <van-pull-refresh v-model="isLoading" @refresh="onRefresh" :success-text='xx'>
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh" :success-text="xx">
       <van-list v-model="upLoading" :finished="finished" finished-text="傻逼，到底了" @load="onLoad">
-        <van-cell v-for="lists in list" :key="lists" :title="lists"></van-cell>
+        <!-- v-for 渲染数据 -->
+        <van-cell v-for="lists in list" :key="lists">
+          <div class="article_item">
+            <h3 class="van-ellipsis">PullRefresh下拉刷新PullRefresh下拉刷新下拉刷新下拉刷新</h3>
+            <!-- 三图模式 -->
+            <div class="img_box">
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+              <van-image class="w33" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <!--  单图模式 -->
+            <div class="img_box">
+              <van-image class="w100" fit="cover" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+            </div>
+            <div class="info_box">
+              <span>你像一阵风</span>
+              <span>8评论</span>
+              <span>10分钟前</span>
+              <span class="close">
+                <van-icon name="cross"></van-icon>
+              </span>
+            </div>
+          </div>
+        </van-cell>
       </van-list>
     </van-pull-refresh>
   </div>
@@ -42,7 +65,7 @@ export default {
     // 下拉加载方法
     onRefresh () {
       setTimeout(() => {
-        let arr = Array.from(Array(10), (value, index) => ('爱情' + (index + 1)))
+        let arr = Array.from(Array(10), (value, index) => '爱情' + (index + 1))
         this.list.unshift(...arr) // 将数据添加到队首
         this.isLoading = false // 关掉下拉状态
         this.xx = `更新了${arr.length}条数据`
@@ -53,4 +76,44 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.article_item {
+  h3 {
+    font-weight: normal;
+    line-height: 2;
+  }
+  .img_box {
+    display: flex;
+    justify-content: space-between;
+    .w33 {
+      width: 33%;
+      height: 90px;
+    }
+    .w100 {
+      width: 100%;
+      height: 180px;
+    }
+  }
+  .info_box {
+    color: #999;
+    line-height: 2;
+    position: relative;
+    font-size: 12px;
+    span {
+      padding-right: 10px;
+      &.close {
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        line-height: 15px;
+        height: 12px;
+        width: 16px;
+        text-align: center;
+        padding-right: 0;
+        font-size: 8px;
+        position: absolute;
+        right: 0;
+        top: 7px;
+      }
+    }
+  }
+}
 </style>
